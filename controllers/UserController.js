0dad96e9c111fbe52669defb19c0aaa3cf86e326
docziadrianmark <docziadrianmark@turr.hu>
2025-09-08 10:55:07 +0200

    Frontend layout, alap muveletek

---
+++ controllers/UserController.js
@@ -0,0 +1,18 @@
+function register() {
+  const registeringName = document.getElementById("nameField").value;
+  const registeringEmail = document.getElementById("emailField").value;
+  const registeringPassword = document.getElementById("passwordField").value;
+  const registeringPasswordConfirm = document.getElementById(
+    "passwordFieldConfirm"
+  ).value;
+}
+
+function login() {}
+
+function logout() {}
+
+function getProfile() {}
+
+function updateProfile() {}
+
+function updatePassword() {}
